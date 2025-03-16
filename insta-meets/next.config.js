2946c45a1b8f002ce400e36d1a3c98d525c64dc0
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2025-03-16 12:36:04 +0200

    feat: add METERED_API_KEY to Next.js configuration

--- insta-meets/next.config.js
+++ insta-meets/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     BACKEND_URL: process.env.BACKEND_URL ,
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID ,
+    METERED_API_KEY: process.env.METERED_API_KEY,
   },
 }
 
