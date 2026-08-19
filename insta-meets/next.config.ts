8a138e4606a4a214a60a59475584f4810f0b8e65
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-08-19 09:53:56 +0200

    next configuration

--- insta-meets/next.config.ts
+++ insta-meets/next.config.ts
@@ -1,10 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
-};
+  reactStrictMode: true,
+  transpilePackages: ["sonner"],
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    METERED_API_KEY: process.env.METERED_API_KEY,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
